fcd8bea5e033f5aae6f5d5e6ad9bc2fe1e38bbbc
Auriel James <aurieljames11@gmail.com>
2024-06-12 15:33:43 +0800

    Add project files.

---
+++ StudentManagerWeb/Controllers/StudentController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentManagerWeb.Data;
+using StudentManagerWeb.Models;
+
+namespace StudentManagerWeb.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public StudentController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+
+        public IActionResult Index()
+        {
+            IEnumerable<Student> objStudentList= _db.Students;
+            return View(objStudentList);
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Student obj)
+        {
+            _db.Students.Add(obj);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ StudentManagerWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentManagerWeb.Models;
+
+namespace StudentManagerWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {    
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
